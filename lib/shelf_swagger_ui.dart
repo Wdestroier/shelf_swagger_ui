097fd14e3df3fdc70b6f77a4e558d5080c71e96e
stones <ssscast@163.com>
2025-09-13 21:38:58 +0800

    upgrade swagger-ui-dist to 5.29.0

--- lib/shelf_swagger_ui.dart
+++ lib/shelf_swagger_ui.dart
@@ -132,12 +132,12 @@ class SwaggerUI {
     content="SwaggerUI"
   />
   <title>$title</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.29.0/swagger-ui.css" />
 </head>
 <body>
 <div id="swagger-ui"></div>
 <script src="https://cdn.jsdelivr.net/npm/js-yaml@4.1.0/dist/js-yaml.min.js"></script>
-<script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
+<script src="https://unpkg.com/swagger-ui-dist@5.29.0/swagger-ui-bundle.js" crossorigin></script>
 
 <script>
   window.onload = () => {
